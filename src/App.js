930fb3a1f8fa701deee2d499cf9ce9478954b265
Jag953 <Jag953@nyu.edu>
2021-08-29 20:07:16 -0700

    init
    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="solHead">SushiSol</h2>
+        <p>Coming soon on Solana</p>
+        <a href="https://twitter.com/SushiSolDotCom" target="_blank">
+            <button className="btn twitter">
+                Twitter
+            </button>
+        </a>
+        <a href="https://discord.gg/Um62E5AF" target="_blank">
+            <button className="btn discord">Discord</button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
